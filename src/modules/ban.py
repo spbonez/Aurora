54d01e69649cc586865b0916bbbe38f1e6072591
Luke <Lukasronsholt@gmail.com>
2016-10-13 01:15:33 +0200

    Added Ban

---
+++ src/modules/ban.py
@@ -0,0 +1,28 @@
+import discord
+
+async def ban(client, message, arg):
+    string = arg.split(' ')
+
+    # Get the members ID
+    member_id = string[0]
+    member_id = member_id.replace('<', '')
+    member_id = member_id.replace('@', '')
+    member_id = member_id.replace('>', '')
+
+    # Remove the ID and combine back to string
+    del string[0]
+    reason = ' '.join(string)
+
+    # Find the member using the ID
+    members = client.get_all_members()
+    for member in members:
+        if member.id == member_id:
+            # Send message to the user, with the reason to the ban
+            await client.send_message(member, 'You have been banned from KvasigSG Development\n'
+                                              'Reason for your ban: ```' + reason + '```')
+            # ban the member
+            await client.send_message(message.channel, '' + member.name + ' was banned, and a personal message'
+                                                       ' was sent to him/her with the '
+                                                       'reason: ```' + reason + '```')
+            await client.ban(member, 2)
+            break
