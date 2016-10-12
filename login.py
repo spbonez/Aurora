7807e463532f419ab3dea3ec04f36cd3cfa8b796
MeCassie <wellsmecassandra@gmail.com>
2016-10-12 08:39:29 +0200

    Create a self init
    To better manage the bot use the self init

--- login.py
+++ login.py
@@ -1,19 +1,25 @@
 import discord
 
 from config.auth import user
-Token = user.Token
+class bot:
+    # Define Bot Init to the use of self.
+    def __init__(self):
+        self.running = True
+        #self.commands = dir to commands
+        #self.prefix = dir to prefix
+        self.Token = user.Token
 
-client = discord.Client()
-
-
-@client.event
-async def on_ready():
-    print('\nLogged in successfully')
-    print('User Name:   ', client.user.name)
-    print('User ID:     ', client.user.id)
-    print('Connected Servers:')
-    for server in client.servers:
+    def start(self):
+        client = discord.Client()
+        @client.event
+        async def on_ready():
+        print('\nLogged in successfully')
+        print('User Name:   ', client.user.name)
+        print('User ID:     ', client.user.id)
+        print('Connected Servers:')
+        for server in client.servers:
         print(server)
-    print('------------------')
+        print('------------------')
 
-client.run(Token)
+        client.run(self.Token)
+bot.start()
