ac8fd28472cf946114bdd8a6a51e0df4a02af0d1
MeCassie <wellsmecassandra@gmail.com>
2016-10-12 09:58:56 +0200

    Fixed syntax mistakes

--- login.py
+++ login.py
@@ -1,23 +1,26 @@
 import discord
 
 from config.auth import user
+
+
 class bot:
     # Define Bot Init to the use of self.
     def __init__(self):
-        #self.commands = dir to commands
-        #self.prefix = dir to prefix
+        # self.commands = dir to commands
+        # self.prefix = dir to prefix
         self.Token = user.Token
 
     def start(self):
         client = discord.Client()
+        
         @client.event
         async def on_ready():
-        print('\nLogged in successfully')
-        print('User Name:   ', client.user.name)
-        print('User ID:     ', client.user.id)
-        print('Connected Servers:')
+            print('\nLogged in successfully')
+            print('User Name:   ', client.user.name)
+            print('User ID:     ', client.user.id)
+            print('Connected Servers:')
         for server in client.servers:
-        print(server)
+            print(server)
         print('------------------')
 
         client.run(self.Token)
