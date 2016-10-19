484dbadbae489bb3cce40ef6cab79b87160a5fcf
sagooper <sagooper@gmail.com>
2016-10-19 16:46:59 +0200

    Fixed #6 - Adding !help command to the bot (#12)

---
+++ src/modules/help.py
@@ -0,0 +1,29 @@
+async def help(client, message, *args):
+    useage = 'Usage:\n'
+    description = 'Description:\n'
+    cmd = args[0]
+
+    if cmd == 'ban':
+        helptext = '```\n!' + cmd + ' [@user] [Message]\n```'
+        destext = '```\n[!' + cmd + '] bans a member, a reason will be sent to the member\n```'
+    elif cmd == 'hi':
+        helptext = '```\n!' + cmd + '\n```'
+        destext = '```\n[!' + cmd + '] bot replies with a hi\n```'
+    elif cmd == 'info':
+        helptext = '```\n!' + cmd + '\n!' + cmd + ' Update [Message]```'
+        destext = '```\n[!' + cmd + '] shows or updates the bot info\n```'
+    elif cmd == 'kick':
+        helptext = '```\n!' + cmd + ' [@user] [Message]\n```'
+        destext = '```\n[!' + cmd + '] kicks a member, a reason will be sent to the member\n```'
+    elif cmd == 'mute':
+        helptext = '```\n!' + cmd + ' [@user]\n```'
+        destext = '```\n[!' + cmd + '] mutes a member\n```'
+    elif cmd == 'roll':
+        helptext = '```\n!' + cmd + '\n```'
+        destext = '```\n[!' + cmd + '] rolls a dice from 1 to 6\n```'
+    else:
+        helptext= '```\n!help [command_name]\n !\n```'
+        destext= '```\n[!help] display the Usage and Description of the command\n```'
+
+    helpmessage = useage + helptext + description + destext
+    await client.send_message(message.channel, helpmessage)
