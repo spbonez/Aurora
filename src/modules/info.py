c44fa3c7868707f21accd1dbe5f6195ff89e6c11
Luke <Lukasronsholt@gmail.com>
2016-10-27 22:14:48 +0200

    Final edits

--- src/modules/info.py
+++ src/modules/info.py
@@ -1,17 +1,21 @@
-import discord
-
+import src.permission as perm
 
 async def info(client, message, *args):
     args = args[0].split(' ')
     if args[0] == 'update':
-        del args[0]
+        if await perm.have_access(message.author) == perm.Roles.Admin.Level:
+            del args[0]
 
-        new_info = ' '.join(args)
-        with open("..\config\information.txt", "wt") as out_file:
-            out_file.write(new_info)
-        out_file.close()
-        await client.send_message(message.channel, 'Information was updated, the new '
-                                                   'information is: ```' + new_info + '```')
+            new_info = ' '.join(args)
+            with open("..\config\information.txt", "wt") as out_file:
+                out_file.write(new_info)
+            out_file.close()
+            await client.send_message(message.channel, 'Information was updated, the new '
+                                                       'information is: ```' + new_info +
+                                                       '```')
+        else:
+            await client.send_message(message.channel, 'This command is only available for '
+                                      + perm.Roles.Admin.Name + 's')
 
     else:
         # Read a file
