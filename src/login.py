dfbf26e7aa36944996c6d6faee5fbf8325418a65
Luke <Lukasronsholt@gmail.com>
2017-04-24 21:02:51 +0200

    Linux and Windows support

--- src/login.py
+++ src/login.py
@@ -1,11 +1,9 @@
-import sys
-sys.path.append('/home/pi/KvasirDevelopment/Aurora/')
 from src import Commands
 import os
 from discord.ext import commands
 from config.auth import User
 
-modulePath = os.getcwd() + "\modules"
+modulePath = os.path.join(os.getcwd(), "src", "modules")
 
 bot = commands.Bot(command_prefix=commands.when_mentioned_or('§'), description='AuroraV2 DevBuild PreAlpha')
 Commands.load_all_modules(modulePath, bot)
