51d2224e25161865eb33c185df681832ccef93e8
Luke <Lukasronsholt@gmail.com>
2017-08-11 23:47:00 +0200

    hehe

--- src/Watcher.py
+++ src/Watcher.py
@@ -7,6 +7,12 @@ from discord.ext import commands
 class Watcher:
 
     @staticmethod
-    def new_message(message):
+    async def new_message(message):
+        Logger.log_to_console(message)
+
+
+class Logger:
+    @staticmethod
+    async def log_to_console(message):
         print(str(message.server) + " | " + str(message.channel) + " | " + str(message.author) + " - "
               + str(message.content))
