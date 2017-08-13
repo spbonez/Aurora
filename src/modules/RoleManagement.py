814a816e3249af5c02d60a1cb237a5d5fb9e08be
Luke <Lukasronsholt@gmail.com>
2017-08-14 01:20:43 +0200

    Role Management

--- src/modules/RoleManagement.py
+++ src/modules/RoleManagement.py
@@ -6,11 +6,24 @@ from discord.ext import commands
 class RoleManagement:
     def __init__(self, bot):
         self.bot = bot
+        self.active = False
 
     @commands.command(pass_context=True, no_pm=True)
     async def activate_roles(self, ctx):
+
+        def yes_or_no(msg):
+            if msg.content == 'yes' or msg.content == 'Yes':
+                return True
+            else:
+                return False
+
         if ctx.message.author is ctx.message.server.owner:
-            print("Yeaah boi")
+            if not self.active:
+                self.bot.say("Do you want roles based on games ? ( yes or no)")
+                msg = self.bot.wait_for_message(timeout=30, author=ctx.message.author, channel=ctx.message.channel,
+                                                check=yes_or_no)
+                if msg is not None:
+                    self.bot.say("YAY!")
 
     async def assign_role(self):
         pass
