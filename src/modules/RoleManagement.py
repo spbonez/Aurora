814c3809b49c4a71dcdf866f9bee07683dafe689
Luke <lukasronsholt@gmail.com>
2017-08-14 00:16:17 +0200

    Role Management

---
+++ src/modules/RoleManagement.py
@@ -0,0 +1,29 @@
+# coding=utf-8
+import discord
+from discord.ext import commands
+
+
+class RoleManagement:
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(pass_context=True, no_pm=True)
+    async def activate_roles(self, ctx, *, server: discord.Server, user: discord.Message.author):
+        if user is server.owner:
+            print("YES!")
+
+    async def assign_role(self):
+        pass
+
+    async def remove_role(self):
+        pass
+
+
+def add_to_bot(bot):
+    bot.add_cog(RoleManagement(bot))
+    print("Role Management Added to Bot!")
+
+
+def remove_from_bot(bot):
+    bot.remove_cog("RoleManagement")
+    print("Role Management Removed From Bot")
