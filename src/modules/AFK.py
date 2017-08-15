9ceb61b9ce2621f978272d6ffc7e347f15ef7cf8
Luke <Lukasronsholt@gmail.com>
2017-08-15 14:52:30 +0200

    AFK module

--- src/modules/AFK.py
+++ src/modules/AFK.py
@@ -10,3 +10,13 @@ class AFK:
     @commands.command(pass_context=True, no_pm=True)
     async def afk(self, ctx, status : str):
         self.bot.change_nickname(ctx.message.author, "[AFK]" + ctx.message.author.nick)
+
+
+def add_to_bot(bot):
+    bot.add_cog(AFK(bot))
+    print("AFK Added to Bot!")
+
+
+def remove_from_bot(bot):
+    bot.remove_cog("AFK")
+    print("AFK Removed From Bot")
