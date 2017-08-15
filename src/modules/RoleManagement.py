80957707830c3c1d8a06835d631249f5e5cf0f9b
Luke <Lukasronsholt@gmail.com>
2017-08-15 19:59:54 +0200

    Goddammit, stupid await again

--- src/modules/RoleManagement.py
+++ src/modules/RoleManagement.py
@@ -40,7 +40,7 @@ class RoleManagement:
         for country in self.countries:
             msg += country + "\n"
         msg += "'''"
-        self.bot.say(msg)
+        await self.bot.say(msg)
 
 
 def add_to_bot(bot):
