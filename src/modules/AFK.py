18512fe9ada43add2fe7df427bf424abbc457236
Luke <Lukasronsholt@gmail.com>
2017-08-15 16:16:24 +0200

    AFK module

--- src/modules/AFK.py
+++ src/modules/AFK.py
@@ -1,17 +1,36 @@
 # coding=utf-8
-import discord
+import discord.utils as utilis
 from discord.ext import commands
 
 
 class AFK:
     def __init__(self, bot):
         self.bot = bot
+        self.afk_members = []
 
     @commands.command(pass_context=True, no_pm=True)
     async def afk(self, ctx):
+        self.afk_members.append(ctx.message.author)
         await self.bot.change_nickname(ctx.message.author, "bob")
 
-'''
+    # Afk member messaged
+    async def message_watcher(self):
+
+        def check(msg):
+            if msg.mentions in self.afk_members:
+                return True
+            else:
+                return False
+
+        if self.afk_members.count > 0:
+            msg = await self.bot.wait_for_message(check=check)
+
+            if msg is not None:
+                await self.bot.say("bob")
+
+    message_watcher()
+
+
 def add_to_bot(bot):
     bot.add_cog(AFK(bot))
     print("AFK Added to Bot!")
@@ -19,4 +38,4 @@ def add_to_bot(bot):
 
 def remove_from_bot(bot):
     bot.remove_cog("AFK")
-    print("AFK Removed From Bot")'''
+    print("AFK Removed From Bot")
